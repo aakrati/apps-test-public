e040e54d8729a07831215bb0eedbdf4eed599833
aakrati <aakrati.talati@databricks.com>
2024-05-07 12:11:21 -0700

    fix

--- gradio_app/app.py
+++ gradio_app/app.py
@@ -4,7 +4,7 @@ import gradio as gr
 app = FastAPI()
 
 def greet(name):
-    return "Hello , " + name + "!"
+    return "Hola , " + name + "!"
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 
